74798398f1bfc15207ed2e5e0a5a6548d1cc4284
leral00 <lwlyuu@mail.ru>
2026-02-26 21:01:10 +0300

    Update config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,1 +1,13 @@
+import json
+class ConfigManager:
 
+    def save(self, data):
+        with open("config.json", "w") as f:
+            json.dump(data, f)
+
+    def load(self):
+        try:
+            with open("config.json", "r") as f:
+                return json.load(f)
+        except:
+            return {}

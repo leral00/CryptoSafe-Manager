37fac824330fb49c6519a6f4addaac54fcd81553
leral00 <lwlyuu@mail.ru>
2026-02-26 21:21:16 +0300

    Update PasswordEntry.py

--- src/gui/widgets/PasswordEntry.py
+++ src/gui/widgets/PasswordEntry.py
@@ -1,1 +1,26 @@
+import tkinter as tk
 
+class PasswordEntry(tk.Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.showing = False
+
+        self.entry = tk.Entry(self, show="*")
+        self.entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+        self.button = tk.Button(self, text="Показать", command=self.toggle)
+        self.button.pack(side=tk.RIGHT)
+
+    def toggle(self):
+        if self.showing:
+            self.entry.config(show="*")
+            self.button.config(text="Показать")
+        else:
+            self.entry.config(show="")
+            self.button.config(text="Скрыть")
+        self.showing = not self.showing
+
+    def get(self):
+        return self.entry.get()

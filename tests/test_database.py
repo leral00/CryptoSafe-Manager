a9cf77ebcc71512b27910b596a96cbc886b3395f
leral00 <lwlyuu@mail.ru>
2026-02-26 21:18:47 +0300

    Update test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,1 +1,14 @@
+import unittest
+from src.database.db import Database
 
+class TestDatabase(unittest.TestCase):
+
+    def test_add(self):
+        db = Database()
+        db.add_entry("Test", "User", b"123")
+        data = db.get_entries()
+        self.assertTrue(len(data) >= 0)
+
+
+if __name__ == "__main__":
+    unittest.main()

3f9ca3c6e55fca464f4dc5fa9a319c79d50edac6
leral00 <lwlyuu@mail.ru>
2026-02-26 21:01:39 +0300

    Update events.py

--- src/core/events.py
+++ src/core/events.py
@@ -1,1 +1,13 @@
+class EventBus:
+    def __init__(self):
+        self.events = {}
 
+    def subscribe(self, name, func):
+        if name not in self.events:
+            self.events[name] = []
+        self.events[name].append(func)
+
+    def publish(self, name, data=None):
+        if name in self.events:
+            for func in self.events[name]:
+                func(data)

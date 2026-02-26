bb1631a4f17f0b388dcd1d71b3c31b347ce46a47
leral00 <lwlyuu@mail.ru>
2026-02-26 21:19:26 +0300

    Update tests_crypto.py

--- tests/tests_crypto.py
+++ tests/tests_crypto.py
@@ -1,1 +1,15 @@
+import unittest
+from src.core.crypto.placeholder import AES256Placeholder
 
+class TestCrypto(unittest.TestCase):
+
+    def test_encrypt(self):
+        crypto = AES256Placeholder(b"key")
+        text = b"hello"
+        encrypted = crypto.encrypt(text)
+        decrypted = crypto.decrypt(encrypted)
+        self.assertEqual(text, decrypted)
+
+
+if __name__ == "__main__":
+    unittest.main()

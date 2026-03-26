fcc21f1db8002e76fc49cb015715f892762c263b
leral00 <lwlyuu@mail.ru>
2026-03-26 22:38:28 +0300

    Update abstract.py

--- src/core/crypto/abstract.py
+++ src/core/crypto/abstract.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 class EncryptionService(ABC):
+    def __init__(self, key_manager):
+        self.key_manager = key_manager
 
     @abstractmethod
-    def encrypt(self, data: bytes, key: bytes) -> bytes:
+    def encrypt(self, data: bytes, key_id: str) -> bytes:
         pass
 
     @abstractmethod
-    def decrypt(self, ciphertext: bytes, key: bytes) -> bytes:
+    def decrypt(self, ciphertext: bytes, key_id: str) -> bytes:
         pass
